5ba2155a9e9f37f0062ac76e1c86c3940f06eedb
maricela renderos <maricelarenderos145@gmail.com>
2025-08-25 17:39:15 -0600

    se agrego una  etiqueta

--- miprimerproyecto/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ miprimerproyecto/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCalcular
@@ -253,11 +254,21 @@
             this.label7.TabIndex = 16;
             this.label7.Text = "MASA";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(287, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "MASA";
+            // 
             // cbxItems
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -301,6 +312,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
