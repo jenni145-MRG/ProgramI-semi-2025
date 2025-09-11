515debf854ad70bbf926eee7043bf58e64543c41
jenni145-MRG <maricelarenderos145@gmail.com>
2025-09-11 10:40:26 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programacionprimerproyecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSaludo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsaludar_Click(object sender, EventArgs e)
+        {
+            lblSaludo.Text = "¡Hola " + txtNombre.Text + "!";
+        }
+    }
+}
