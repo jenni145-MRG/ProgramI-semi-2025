2e52b257bf676614fd54cd108a47af25e49ccb8a
maricela renderos <maricelarenderos145@gmail.com>
2025-09-12 16:43:20 -0600

    creacion de clases y BD

---
+++ miprimerproyecto/parcialprogramI/parcialprogramI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace parcialprogramI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Location = new System.Drawing.Point(259, 141);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(72, 16);
+            this.lblMonto.TabIndex = 0;
+            this.lblMonto.Text = "Resultado:";
+            this.lblMonto.Click += new System.EventHandler(this.lblMonto_Click);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(252, 65);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(160, 22);
+            this.txtMonto.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(252, 175);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(123, 47);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(137, 65);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(109, 16);
+            this.lbResultado.TabIndex = 3;
+            this.lbResultado.Text = "ingrese el monto:";
+            this.lbResultado.Click += new System.EventHandler(this.lbResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblMonto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbResultado;
+    }
+}
+
---
+++ miprimerproyecto/parcialprogramI/parcialprogramI/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parcialprogramI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double[] desde = new double[]
+{
+      0.01, 500.01, 1000.01, 2000.01, 3000.01, 8000.01, 18000.01, 30000.01, 60000.01, 100000.01, 200000.01, 300000.01, 400000.01, 500000.01, 1000000.01
+};
+
+        double[] hasta = new double[]
+        {
+         500, 1000, 2000, 3000, 6000, 18000, 30000, 600000, 100000, 200000, 300000, 400000, 500000, 1000000, 99999999
+        };
+
+        double[] precioAd = new double[]
+        {
+         0, 3, 3, 3, 3, 2, 2, 1, 0.8, 0.6, 0.6, 0.45, 0.4, 0.3, 0.18
+        };
+
+
+        private void form1_load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double monto = double.Parse(txtMonto.Text);
+            for (int i = 0; i < desde.Length; i++)
+            {
+                if (monto >= desde[i] && monto <= hasta[i])
+                {
+                    double total = (monto / hasta[i]) * precioAd[i] + precioAd[i];
+                    lblMonto.Text = "El impuesto a pagar es: " + total.ToString("F2");
+                }
+            }
+        }
+
+                
+
+
+        
+        private void lbResultado_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+                
+
+        private void txtMonto_TextChanged(double monto )
+        {
+            
+
+                
+            
+          
+    }
+
+        private void lblMonto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
